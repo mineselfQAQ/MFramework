1821bec6bc9fb787384cb626c29e19470864613f
mineself <mineself0817@gmail.com>
2024-03-04 23:16:40 +0800

    2024_03_04_02

--- Assets/MFramework_Test/Path/Test_FindScriptPath.cs
+++ Assets/MFramework_Test/Path/Test_FindScriptPath.cs
@@ -7,25 +7,24 @@ public class Test_FindScriptPath : MonoBehaviour
 {
     private void Start()
     {
-        //string name = this.name;
-        //string[] guids = AssetDatabase.FindAssets(name);
-        //foreach (string guid in guids)
-        //{
-        //    string path = AssetDatabase.GUIDToAssetPath(guid);
-        //    Object obj = AssetDatabase.LoadAssetAtPath(path, typeof(MonoScript));
+        //寻找该脚本存储的实际位置
+        string name = this.name;
+        string[] guids = AssetDatabase.FindAssets(name);
+        foreach (string guid in guids)
+        {
+            string path = AssetDatabase.GUIDToAssetPath(guid);
+            Object obj = AssetDatabase.LoadAssetAtPath(path, typeof(MonoScript));
 
-        //    string secondPath = "";
-        //    //secondPath = AssetDatabase.GetAssetPath(obj); //等价式
-        //    if (obj)
-        //    {
-        //        secondPath = path;
-        //        string fullPath = Path.GetFullPath(secondPath);
-        //        Log.Print(fullPath);
-        //        //EditorGUIUtility.PingObject(obj);
-        //        break;
-        //    }
-        //}
-
-        //EditorGUIUtility.PingObject(obj);
+            string secondPath = "";
+            //secondPath = AssetDatabase.GetAssetPath(obj); //等价式
+            if (obj)
+            {
+                secondPath = path;
+                string fullPath = Path.GetFullPath(secondPath);
+                Log.Print(fullPath);
+                //EditorGUIUtility.PingObject(obj); //不能Ping，必须在非运行时才能起效
+                break;
+            }
+        }
     }
 }
