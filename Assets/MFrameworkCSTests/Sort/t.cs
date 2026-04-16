1f836ccbef072fd42f2716bd7f980bf93696d144
mineself <mineself0817@gmail.com>
2026-04-16 22:18:03 +0800

    #-1

--- Assets/MFrameworkCSTests/Sort/t.cs
+++ Assets/MFrameworkCSTests/Sort/t.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 
 using MFrameworkCSTests.Sort;
@@ -8,43 +9,10 @@ namespace MFrameworkCSTests.Sort11
     {
         public void Sort(IList<T> list, IComparer<T> comparer)
         {
-            for (int i = list.Count / 2 - 1; i >= 0; i--)
-            {
-                SiftDown(list, comparer, i, list.Count - 1);
-            }
+            Dictionary<int, int> dic = new Dictionary<int, int>();
 
-            int n = 0;
-            for (int i = list.Count - 1; i > 0; i--)
-            {
-                SortUtils.Swap(list, 0, list.Count - 1 - n);
-                SiftDown(list, comparer, 0, i);
-                n++;
-            }
-        }
-
-        public void SiftDown(IList<T> list, IComparer<T> comparer, int i, int n)
-        {
-            while (i < n)
-            {
-                int left = i * 2 + 1;
-                if (left >= n) return;
-                int right = i * 2 + 2;
-
-                int index;
-                if (right < n && comparer.Compare(list[right], list[left]) > 0)
-                {
-                    index = right;
-                }
-                else
-                {
-                    index = left;
-                }
-
-                if (comparer.Compare(list[i], list[index]) > 0) return;
-
-                SortUtils.Swap(list, i, index);
-                i = index;
-            }
+            dic[1] = 1;
+            dic[1] = 2;
         }
     }
 }
--- Assets/MFrameworkExamples/Framework/MEntry.cs
+++ Assets/MFrameworkExamples/Framework/MEntry.cs
@@ -2,14 +2,12 @@ using MFramework.Core;
 using MFramework.Core.CoreEx;
 using MFramework.Core.IOC;
 using MFramework.Core.Tracker;
-using UnityEngine;
 using IServiceProvider = MFramework.Core.CoreEx.IServiceProvider;
 
 namespace MFrameworkExamples.Framework
 {
     public class MEntry : MEntryBase
     {
-        public GameObject go;
         protected override void OnBootstrapping(TrackerStartedEvent e)
         {
             MLog.Default.D($"开始启动 时间：{e.StartTime}");
@@ -24,11 +22,11 @@ namespace MFrameworkExamples.Framework
         {
             MLog.Default.D($"开始初始化 时间：{e.StartTime}");
         }
-        
+
         protected override void OnInitialized(TrackerStoppedEvent e)
         {
             MLog.Default.D($"初始化完成 时间：{e.EndTime}");
-            
+
             var container = MIOCContainer.Default;
             container.Resolve<A>().Print();
         }
