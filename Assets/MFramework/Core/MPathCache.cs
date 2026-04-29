0e0bd819a9dbb79fb324ca6d4c59a41126e8d652
mineself <mineself0817@gmail.com>
2026-04-29 11:20:46 +0800

    [2026_04_29][chore] 路径小修改

--- Assets/MFramework/Core/MPathCache.cs
+++ Assets/MFramework/Core/MPathCache.cs
@@ -5,15 +5,15 @@ namespace MFramework.Core
 {
     public static class MPathCache
     {
-        /// <summary>
-        /// PC根目录(包括Editor/Build)
-        /// </summary>
-        public static readonly string PC_ROOT_PATH = Path.GetDirectoryName(Application.dataPath);
-
         public static readonly string ASSET_PATH = Application.dataPath;
         public static readonly string STREAMING_ASSETS_PATH = Application.streamingAssetsPath;
         public static readonly string PERSISTENT_DATA_PATH = Application.persistentDataPath;
         public static readonly string TEMPORARY_CACHE_PATH = Application.temporaryCachePath;
+
+        /// <summary>
+        /// PC根目录(包括Editor/Build)
+        /// </summary>
+        public static readonly string PC_ROOT_PATH = Path.GetDirectoryName(ASSET_PATH);
 
         public static readonly string DEFAULT_XML_PATH = $"{PERSISTENT_DATA_PATH}/XmlSettings";
         public static readonly string DEFAULT_JSON_PATH = $"{PERSISTENT_DATA_PATH}/JsonSettings";
